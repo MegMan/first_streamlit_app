3554b0b09543afee265abd0ed1b239b126aab907
MegMan <118852275+MegMan@users.noreply.github.com>
2022-11-22 15:27:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My Parents Healthy New Dinner')
 streamlit.title('Breakfast Menu')
 streamlit.text('Blueberry Oats')
+streamlit.text('Kale Spinach Smoothis')
+streamlit.text('Hard Boiled Free Range Eggs')
